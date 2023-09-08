e7cb9176f4d6e4e0a32e95d34b93152ddb7637f7
Robin M <robin59530@gmail.com>
2023-09-08 09:00:02 -0400

    Intégration de Leaflet avec HeatMap, Map contenant des points (Aléatoire) sur Montréal

--- prototype_HeatMap/public/js/main.js
+++ prototype_HeatMap/public/js/main.js
@@ -1,19 +1,34 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const heatmapInstance = h337.create({
-        container: document.querySelector('#heatmap'),
-        radius: 10
-    });
+    // Initialisez une carte Leaflet
+    const map = L.map('mapid').setView([45.50884, -73.58781], 8); // Coordonnées pour Montréal
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+    }).addTo(map);
+
+    // Configuration pour la couche de heatmap
+    const heatmapConfig = {
+        radius: 0.01,
+        maxOpacity: 0.8, 
+        scaleRadius: true, 
+        useLocalExtrema: true,
+        latField: 'lat',
+        lngField: 'lng',
+        valueField: 'value'
+    };
+
+    const heatmapLayer = new HeatmapOverlay(heatmapConfig);
+    map.addLayer(heatmapLayer);
 
     const points = [];
     const max = 100;
     const width = 500;
     const height = 500;
 
-    while (points.length < 100) {
+    while (points.length < 10) {
         const val = Math.floor(Math.random() * 100);
         const point = {
-            x: Math.floor(Math.random() * width),
-            y: Math.floor(Math.random() * height),
+            lat: 45.50884 + (Math.random() - 0.5) * 0.1, // Générer des coordonnées aléatoires autour de Montréal
+            lng: -73.58781 + (Math.random() - 0.5) * 0.1,
             value: val
         };
         points.push(point);
@@ -24,5 +39,5 @@ document.addEventListener('DOMContentLoaded', () => {
         data: points
     };
 
-    heatmapInstance.setData(data);
+    heatmapLayer.setData(data);
 });
